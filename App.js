80659a38be14854da9551146ae123980851c9d33
unknown <joel.buti97@gmail.com>
2022-10-06 18:00:25 +0200

    fix screens

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Home from './components/Home'
+import Home from './Screens/Login'
 
 const Stack = createNativeStackNavigator();
 export default function App() {
