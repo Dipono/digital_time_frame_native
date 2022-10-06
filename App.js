ff72434a30cc675b6b4f84c4fe4b48a784044eaf
dipono <joel.buti97@gmail.com>
2022-10-06 15:47:50 +0200

    added screen

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './components/Home'
+
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}>
+          {/* Screen Home Page */}
+        <Stack.Screen
+          name="home"
+          component={Home}
+        />
+        {/* More Screens hereW */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/Login.js
@@ -0,0 +1,9 @@
+import {Text, View } from 'react-native';
+function Home() {
+    return(
+        <Text>Login</Text>
+    )
+
+}
+
+export default Home;
