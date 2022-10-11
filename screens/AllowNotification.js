a771d2c01625298141e8aa6ce72d192313a30cba
Thorbileh <preciousthobile99@gmail.com>
2022-10-11 11:58:06 +0200

    //allow notificatioin

--- screens/AllowNotification.js
+++ screens/AllowNotification.js
@@ -1,21 +1,30 @@
 import { Image, SafeAreaView, Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 
-const img = require('../assets/image/undraw_wall_post_re_y78d (1).svg')
+const img = require('../assets/image/undraw_my_notifications_re_ehmk.svg')
+const backImg = require('../assets/image/back.png')
 function AllowNotification() {
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.heading}>Notifications</Text>
+            <View style={styles.headingView}>
+                <Image style={styles.backIcon} source={backImg} />
+                <Text style={styles.heading}>Notifications</Text>
+            </View>
+
             <Image style={styles.image} source={img} />
+
             <View style={styles.message}>
                 <Text>May include sounds,alerts and icons</Text>
                 <Text>App requirements Permission to send notifications</Text>
             </View>
+
             <TouchableOpacity style={styles.allowButton}>
                 <Text style={styles.allowButtonText}>Allow</Text>
             </TouchableOpacity>
+
             <TouchableOpacity style={styles.skipButton}>
                 <Text style={styles.skipText}>Skip</Text>
             </TouchableOpacity>
+
         </SafeAreaView>
     )
 
@@ -26,9 +35,17 @@ const styles = StyleSheet.create({
         backgroundColor: '#E7E7E7',
         textAlign: 'center',
     },
+    headingView: {
+        flexDirection: 'row',
+        marginTop: 30,
+    },
+    backIcon: {
+        width: 25,
+        height: 20,
+        marginLeft:'2%'
+    },
     heading: {
-        margin: 'auto',
-
+        margin:'auto',
         fontFamily: 'Emblema One',
         fontStyle: 'normal',
         fontWeight: 400,
@@ -37,20 +54,22 @@ const styles = StyleSheet.create({
     image: {
         width: 365,
         height: 351,
-        margin:'auto',
+        resizeMode: 'contain',
+        alignSelf: 'center',
+
     },
     message: {
         width: 255,
-        margin: 'auto',
-
+        alignSelf: 'center',
+        marginTop: -20,
         fontFamily: 'Ebrima',
         fontStyle: 'normal',
         fontWeight: 400,
         fontSize: 16,
-
+marginTop:10,
     },
     allowButtonText: {
-        
+
         margin: 'auto',
         fontFamily: 'Emblema One',
         fontStyle: 'normal',
@@ -63,8 +82,8 @@ const styles = StyleSheet.create({
     allowButton: {
         width: 133,
         height: 54,
-        
-        margin: 'auto',
+        alignSelf: 'center',
+        marginTop:18,
         backgroundColor: '#308989',
         borderRadius: 20,
     },
@@ -74,6 +93,7 @@ const styles = StyleSheet.create({
         fontWeight: 400,
         fontSize: 24,
         lineHeight: 29,
+        marginTop:10,
     }
 })
 
