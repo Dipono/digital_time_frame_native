bcd77ff4711ea412c4bd1debc0db2709b85ac145
unknown <joel.buti97@gmail.com>
2022-10-11 06:47:50 +0200

    qr code initial commit

---
+++ screens/QRCode.js
@@ -0,0 +1,10 @@
+import {Text, View } from 'react-native';
+//import { RNQRCodeScannerProps, RNQRCodeScannerState  } from 'react-native-qrcode-scanner'
+function QRCode() {
+    return(
+        <Text>QRCode</Text>
+    )
+
+}
+
+export default QRCode;
