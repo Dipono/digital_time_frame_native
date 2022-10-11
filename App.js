8c71f16b6c0e2dd57a3e98b0f7b625f482fba330
unknown <mphogiftzondo@gmail.com>
2022-10-11 11:55:40 +0200

    ClockIn page with upload popUp

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Home from './screens/Login'
+import Home from './screens/ClockInPage'
 
 const Stack = createNativeStackNavigator();
 export default function App() {
