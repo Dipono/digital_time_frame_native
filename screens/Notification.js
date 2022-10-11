1d2f6158303c39f4272a828b97ad14b7fa7da6df
dipono <joel.buti97@gmail.com>
2022-10-11 16:02:52 +0200

    no notification functional

--- screens/Notification.js
+++ screens/Notification.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Image, TouchableOpacity, Modal, Pressable, ScrollView } from 'react-native';
 function Notification() {
 
@@ -18,16 +18,19 @@ function Notification() {
         { id: 7, nameFrom: 'admin', title: 'workshop', message: `As a teacher, I've internalized this message. Every day, all around us, we see the consequences of silence manifest themselves in the form of discrimination, violence, genocide and war. In the classroom, I challenge my students to explore the silences in their own lives through poetry. We work together to fill those spaces, to recognize them, to name them, to understand that they don't have to be sources of shame. In an effort to create a culture within my classroom where students feel safe sharing the intimacies of their own silences, I have four core principles posted on the board that sits in the front of my class, which every student signs at the beginning of the year: read critically, write consciously, speak clearly, tell your truth.`, dateTime: '05-Sep-2022 12:15' },
 
     ])
+
     const [NotificationSelected, setNotificationSelected] = useState({})
     const [popupNotification, setpopupNotification] = useState(false)
-
+    const [CountNotifications, setCountNotifications] = useState(0)
+    useEffect(()=>{
+        setCountNotifications(1)
+    })
     function removeNotification(message) {
         setNotifications(Notifications.filter((response) => response.id !== message.id));
     }
 
     function SelectedNotification(notification) {
         setNotificationSelected(notification)
-
         setpopupNotification(true)
     }
 
@@ -37,7 +40,7 @@ function Notification() {
                 <View style={styles.inner_modal}>
                     <View style={styles.modal_header}>
                         <Pressable onPress={() => setpopupNotification(false)} >
-                            <Text>X</Text>
+                            <Text style={styles.cancel}>X</Text>
                         </Pressable>
                     </View>
 
@@ -61,19 +64,18 @@ function Notification() {
         </Modal>
     </View>
 
-    return (
-        <ScrollView>
+    let AllNotifications =<ScrollView>
             <View style={styles.notification}>
                 {Notifications.map((messages, xid) => (
                     <View key={xid} style={styles.notificationMessages}>
-                        <View style={styles.icon} >
+                        <View style={[styles.icon, { backgroundColor: xid % 2 === 0 ? 'rgba(222, 0, 0, 1)' : 'rgba(0, 144, 209, 1)', height: '100%' }]} >
                             <Pressable onPress={() => SelectedNotification(messages)}>
-                                <Image source={require('../assets/image/mail-svgrepo-com.svg')} style={styles.mailIcon} />
+                                <Image source={require('../assets/image/chat.png')} style={styles.mailIcon} />
                             </Pressable>
 
                         </View>
 
-                        <View style={styles.summary}>
+                        <View style={[styles.summary, { backgroundColor: xid % 2 === 0 ? 'rgba(255, 225, 225, 1)' : 'rgba(140, 223, 223, 1)', height: '100%' }]}>
                             <Pressable onPress={() => SelectedNotification(messages)}>
                                 <Text style={styles.messageSender}>{messages.nameFrom}</Text>
                                 <Text style={styles.messageTitle}>{messages.title}</Text>
@@ -81,7 +83,7 @@ function Notification() {
 
                         </View>
 
-                        <View style={styles.removeMessage}>
+                        <View style={[styles.removeMessage, { backgroundColor: xid % 2 === 0 ? 'rgba(255, 225, 225, 1)' : 'rgba(140, 223, 223, 1)', height: '100%' }]}>
                             <TouchableOpacity onPress={() => removeNotification(messages)}>
                                 <Image source={require('../assets/image/delete.png')} style={styles.deleteIcon} />
                             </TouchableOpacity>
@@ -92,6 +94,19 @@ function Notification() {
             </View>
         </ScrollView>
 
+    let emptyNotification = <View>
+        <Image source={require('../assets/image/undraw_my_notifications_re_ehmk(1).svg')} style={styles.emptyNoti} />
+        <View>
+            <Text style={styles.emptyTextNotification}>You have no notifications</Text>
+        </View>
+    </View>
+
+    return (
+        <View>
+            {(CountNotifications === 0) && (emptyNotification)}
+            {CountNotifications !==0 && (AllNotifications)}
+        </View>
+
     )
 }
 
@@ -101,14 +116,19 @@ const styles = StyleSheet.create({
     },
     notificationMessages: {
         flexDirection: 'row',
-        margin: 15,
         borderRadius: 10,
-        backgroundColor: 'grey',
-        paddingTop: 15,
-        paddingBottom: 15
+        paddingBottom: 10,
+        marginLeft: 10,
+        marginRight: 10,
+        borderTopLeftRadius: 10
     },
     icon: {
         width: '30%',
+        height: '100%',
+        borderTopLeftRadius: 10,
+        borderBottomLeftRadius: 10,
+        textAlign: 'center',
+        alignItems: 'center',
     },
     mailIcon: {
         height: 60,
@@ -116,34 +136,45 @@ const styles = StyleSheet.create({
     },
     summary: {
         width: '50%',
-        marginTop: 10
     },
     messageSender: {
         fontSize: 15,
         fontWeight: 'bold',
-        marginLeft: 10
+        marginLeft: 10,
+        marginTop: 5
     },
     messageTitle: {
         fontSize: 15,
         marginLeft: 10
+
     },
     removeMessage: {
         width: '20%',
-        marginLeft: 15,
-        marginTop: 10
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10
     },
     deleteIcon: {
         height: 30,
         width: 30,
+        marginLeft: 10,
+        marginTop: 15
     },
     modal: {
         backgroundColor: 'grey',
         flex: 1,
+        marginLeft: 50,
+        marginRight: 50,
+        marginBottom: 50,
+        marginTop: 100,
     },
     inner_modal: {
         backgroundColor: 'white',
         flex: 1,
-        margin: 50,
+    },
+    cancel: {
+        fontWeight: 'bold',
+        fontSize: 25,
+        marginLeft: 5
     },
     modal_header: {
         textAlign: 'right',
@@ -154,42 +185,52 @@ const styles = StyleSheet.create({
     },
     modaTitle: {
         fontSize: 20,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
     },
     modalDateTime: {
         padding: 10
     },
     modalDocument: {
-        width: '50%',
         height: '25%',
         margin: 'auto',
-        backgroundColor: 'red',
+        alignItems: 'center',
     },
     modalAttach: {
-        height: 80,
-        width: 100,
+        height: 100,
+        width: '50%',
     },
     scrollDetails: {
-        marginTop: 15,
-        minHeight: 200
+        marginTop: 10,
+        flex: 1,
+        paddingBottom: 250
     },
     messageDetails: {
         margin: 5,
-        height: 80
+        height: 300
     },
     okClose: {
-        marginLeft: 150,
         backgroundColor: 'blue',
         width: 70,
         height: 40,
         textAlign: 'center',
         borderRadius: 12,
+        marginTop: 15
     },
     okCloseText: {
         marginTop: 10,
-        color: 'white'
-    }
+        color: 'white',
+        textAlign: 'center',
+    },
 
+    emptyNoti:{
+        height: 300,
+        width: 300,
+        margin:50
+    },
+    emptyTextNotification:{
+        fontSize:15,
+        textAlign: 'center',
+    }
 
 })
 
