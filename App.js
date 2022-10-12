6bab03b4958db372a220f934c70fc28146698fea
Thorbileh <preciousthobile99@gmail.com>
2022-10-12 15:34:35 +0200

    //Allow user to change a default password

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Home from './screens/Login'
-
+import DefaultPassword from './screens/DefaultPassword';
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
@@ -15,11 +15,15 @@ export default function App() {
           headerShown: false
         }}>
           {/* Screen Home Page */}
-        <Stack.Screen
+       {/*  <Stack.Screen
           name="home"
           component={Home}
-        />
+        /> */}
         {/* More Screens hereW */}
+        <Stack.Screen
+          name="DefaultPassword"
+          component={DefaultPassword}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ screens/DefaultPassword.js
@@ -0,0 +1,124 @@
+import { Image, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+
+
+
+const backIcon = require('../assets/image/back.png')
+
+export default function DefaultPassword() {
+    return (
+        <SafeAreaView>
+
+            <View style={styles.HeaderView}>
+                <Image source={backIcon} style={styles.backIcon} />
+                <Text style={styles.header}>Change Password</Text>
+            </View>
+
+            <View style={styles.newPassword}>
+                <Text style={styles.text}>New Password:</Text>
+                <TextInput style={styles.input}></TextInput>
+            </View>
+            <View style={styles.confirmPassword}>
+                <Text style={styles.text}>Confirm Password:</Text>
+                <TextInput style={styles.input}></TextInput>
+            </View>
+            <Text style={styles.message}>Please change your default passwod for security</Text>
+
+            <TouchableOpacity style={styles.changeButton} >
+                <Text style={styles.changeText}>Change</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#E7E7E7',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    HeaderView: {
+        flexDirection: 'row',
+        marginTop: 35,
+    },
+    backIcon: {
+        width: 25,
+        height: 20,
+        marginLeft: '2%'
+    },
+    header: {
+        marginLeft: 60,
+        /* fontFamily: 'Emblema One', */
+        fontStyle: 'normal',
+        fontWeight: '400',
+        fontSize: 20,
+        lineHeight: 24,
+    },
+    text: {
+        width: 203,
+        height: 26,
+
+        /* fontFamily: 'Ebrima', */
+        fontStyle: 'normal',
+        fontWeight: '700',
+        fontSize: 20,
+        lineHeight: 26,
+
+    },
+    input: {
+        width: 300,
+        height: 34,
+        boarderColor: 'black',
+        background: '#FFFFFF',
+        borderRadius: 5,
+        padding: 10,
+        marginLeft: -10,
+        marginTop:10
+    },
+    newPassword: {
+        marginTop: 140,
+        alignSelf: 'center',
+        marginLeft: 40
+
+    },
+    confirmPassword: {
+        marginTop: 20,
+        alignSelf: 'center',
+        marginLeft: 40
+    },
+    message: {
+        width: 254,
+        height: 64,
+
+        /* fontFamily: 'Ebrima', */
+        fontStyle: 'normal',
+        fontWeight: '400',
+        fontSize: 18,
+        lineHeight: 24,
+        alignSelf: 'center',
+        marginTop:30,
+    },
+    changeButton: {
+        width: 133,
+        height: 54,
+
+        backgroundColor: '#308989',
+        borderRadius: 20,
+        alignSelf:'center',
+        marginTop:10,
+    },
+    changeText: {
+        width: 107,
+        height: 29,
+
+        /* fontFamily: 'Emblema One', */
+        fontStyle: 'normal',
+        fontWeight: '400',
+        fontSize: 24,
+        lineHeight: 29,
+
+        color: '#FFFFFF',
+        marginLeft: 20,
+        marginTop: 10,
+    }
+});
