86f40755f35057468a7ee989b560be28bb0cf49c
Cardw3ll <105672711+Cardw3ll@users.noreply.github.com>
2022-10-13 13:50:16 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,35 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Logs from './Logs';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import Home from './screens/Login'
-
-const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false
-        }}>
-          {/* Screen Home Page */}
-        <Stack.Screen
-          name="home"
-          component={Home}
-        />
-        {/* More Screens hereW */}
-      </Stack.Navigator>
-    </NavigationContainer>
+   
+     <Logs/>
+   
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
